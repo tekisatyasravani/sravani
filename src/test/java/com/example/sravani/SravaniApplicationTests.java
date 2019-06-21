69903a206ef82b88b59efe16cfc976d36c437cfb
unknown <sathisusmitha@gmail.com>
2019-06-21 15:20:02 +0530

    first commit

---
+++ src/test/java/com/example/sravani/SravaniApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.sravani;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SravaniApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
